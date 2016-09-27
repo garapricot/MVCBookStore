11505f0632502cb46ba1514e8518a5841d96c627
Garegin Hambardzumian <yerevanrockcity@gmail.com>
2016-09-27 14:17:02 +0400

    design commit

--- MyMVCBookStore/Controllers/HomeController.cs
+++ MyMVCBookStore/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Dal;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,40 @@ namespace MyMVCBookStore.Controllers
 {
     public class HomeController : Controller
     {
-
+        private readonly BookService _service = new BookService();
         public ActionResult Index()
         {
-            return View();
+            List<BookViewModel> result = null;
+            try
+            {
+                result = _service.GetAllBook();
+            }
+            catch
+            {
+                return View("Error");
+            }
+            return View(result);
+        }
+        public ActionResult Search(string searchString)
+        {
+            List<BookViewModel> result = null;
+            try
+            {
+                result = _service.GetSearchResult(searchString);
+            }
+            catch
+            {
+                return View("Error");
+            }
+            return View("Index", result);
+        }
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                _service.Dispose();
+            }
+            base.Dispose(disposing);
         }
     }
 }
